606d6afdadd0e2b1083bfdc38d1fbb0ec7c7f76e
Frank Moreno <frankmoreno1993@gmail.com>
2020-04-10 14:34:00 -0500

    Preparando a Opcion para que sea seleccionable

--- lib/widget/listado_opciones.dart
+++ lib/widget/listado_opciones.dart
@@ -1,15 +1,14 @@
+import 'package:app_despliege_dc/colors/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-
 class ListaOpciones extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Positioned(
       top: 110,
       left: 6,
-        child: ScrollOpciones(),
+      child: ScrollOpciones(),
     );
   }
 }
@@ -22,86 +21,97 @@ class ScrollOpciones extends StatelessWidget {
       height: 120,
       child: PageView(
         pageSnapping: false,
-        controller: PageController(
-          initialPage: 1,
-          viewportFraction: 0.3
-        ),
+        controller: PageController(initialPage: 1, viewportFraction: 0.3),
         children: <Widget>[
-          Opcion(
-            color: Color(0xff1AA7E8),
-            icon: FaIcon(FontAwesomeIcons.user,size: 22,color: Colors.white,),
+          Opcion.seleccion(
+            icon: FontAwesomeIcons.user,
             titulo: 'Opcion1',
+            seleccionado: true,
           ),
-          Opcion(
-            color: Colors.grey[350],
-            colorTexto: Colors.black,
-            icon: FaIcon(FontAwesomeIcons.facebook,size: 22,color: Colors.black,),
+          Opcion.seleccion(
+            icon: FontAwesomeIcons.facebook,
             titulo: 'Opcion2',
+            seleccionado: false,
           ),
-          Opcion(
-            color: Colors.grey[350],
-            colorTexto: Colors.black,
-            icon: FaIcon(FontAwesomeIcons.twitch,size: 22,color: Colors.black,),
-            titulo: 'Opcion2',
+          Opcion.seleccion(
+            icon: FontAwesomeIcons.twitch,
+            titulo: 'Opcion3',
+            seleccionado: false,
           ),
-          Opcion(
-            color: Colors.grey[350],
-            colorTexto: Colors.black,
-            icon: FaIcon(FontAwesomeIcons.twitter,size: 22,color: Colors.black,),
-            titulo: 'Opcion2',
+          Opcion.seleccion(
+            icon: FontAwesomeIcons.twitter,
+            titulo: 'Opcion4',
+            seleccionado: false,
           ),
-          Opcion(
-            color: Colors.grey[350],
-            colorTexto: Colors.black,
-            icon: FaIcon(FontAwesomeIcons.chair,size: 22,color: Colors.black,),
-            titulo: 'Opcion2',
+          Opcion.seleccion(
+            icon: FontAwesomeIcons.chair,
+            titulo: 'Opcion5',
+            seleccionado: false,
           ),
-         
-          
-             
         ],
       ),
-      );
+    );
   }
 }
 
 class Opcion extends StatelessWidget {
-
   final FaIcon icon;
   final String titulo;
   final Color color;
   final Color colorTexto;
 
-  const Opcion({ 
-    @required this.icon, 
+  const Opcion({
+    @required this.icon,
     @required this.titulo,
     this.color = Colors.grey,
     this.colorTexto = Colors.white,
-
   });
+
+  factory Opcion.seleccion({
+    bool seleccionado = false,
+    IconData icon,
+    String titulo,
+  }) {
+    assert(icon != null);
+    assert(titulo != null);
+    assert(seleccionado != null);
+    final color = seleccionado ? AppColors.blue : Colors.grey[350];
+    final colorTexto = seleccionado ? Colors.white : Colors.black;
+    return Opcion(
+      titulo: titulo,
+      icon: FaIcon(icon, size: 22, color: colorTexto),
+      color: color,
+      colorTexto: colorTexto,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(10),
-
-      child: Container(
-      padding: EdgeInsets.only(top:20,left: 20),
-        decoration: BoxDecoration(
-          color: this.color,
-          borderRadius: BorderRadius.circular(20),
-
-        ),
-        child: Column(
-          
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            this.icon,
-            SizedBox(height: 3,),
-            Expanded(child: Text(this.titulo,style: TextStyle(fontSize: 18,color: this.colorTexto),)),
-            Expanded(child: Text(this.titulo,style: TextStyle(fontSize: 12,color: this.colorTexto),))
-          ],
-        ),
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.only(top: 20, left: 20),
+      decoration: BoxDecoration(
+        color: this.color,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          this.icon,
+          SizedBox(height: 3),
+          Expanded(
+            child: Text(
+              this.titulo,
+              style: TextStyle(fontSize: 18, color: this.colorTexto),
+            ),
+          ),
+          Expanded(
+            child: Text(
+              this.titulo,
+              style: TextStyle(fontSize: 12, color: this.colorTexto),
+            ),
+          )
+        ],
       ),
     );
   }
