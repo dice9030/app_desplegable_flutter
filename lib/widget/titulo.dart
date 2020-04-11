8675d981166ab9ed22323e4adfd5321e9430d8e0
Frank Moreno <frankmoreno1993@gmail.com>
2020-04-10 21:50:54 -0500

    Titulo es PreferredSizeWidget para funcionar como AppBar

--- lib/widget/titulo.dart
+++ lib/widget/titulo.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class Titulo extends StatelessWidget {
+class Titulo extends StatelessWidget with PreferredSizeWidget {
+  @override
+  Size get preferredSize => Size.fromHeight(60);
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -9,10 +12,18 @@ class Titulo extends StatelessWidget {
         padding: EdgeInsets.only(top: 15),
         child: Row(
           children: <Widget>[
-            SizedBox(width: 20,),
-            Expanded(child: Text('Mi usuario',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),)),
-            FaIcon(FontAwesomeIcons.userCircle,size: 40,),
-            SizedBox(width: 20,),
+            SizedBox(width: 20),
+            Expanded(
+              child: Text(
+                'Mi usuario',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+            ),
+            FaIcon(
+              FontAwesomeIcons.userCircle,
+              size: 40,
+            ),
+            SizedBox(width: 20),
           ],
         ),
       ),
