ca82a85983e99a704064b0f3366e5cce9b8f82f4
Frank Moreno <frankmoreno1993@gmail.com>
2020-04-10 21:57:17 -0500

    Haciendo legible el código así no sea usado

--- lib/widget/listado.dart
+++ lib/widget/listado.dart
@@ -6,86 +6,82 @@ class PerfilUsuario extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
+        DatosPersonales(),
+        SizedBox(height: 10),
         Column(
-        children: <Widget>[
-
-          DatosPersonales(),
-          SizedBox(height: 10,),
-          Column(
-            children: <Widget>[
-              CuadroReserva(),
-              Divider(),
-              ListTile(
-                onTap: (){},
-                leading: FaIcon(FontAwesomeIcons.moneyBillWave),
-                title: Text('Monedero'),
-                trailing: FaIcon(FontAwesomeIcons.chevronRight),
-              ),
-              Divider(),
-              ListTile(
-                onTap: (){},
-                leading: FaIcon(FontAwesomeIcons.moneyCheckAlt),
-                title: Text('Cupones'),
-                trailing: FaIcon(FontAwesomeIcons.chevronRight),
-              ),
-              Divider(),
-              ListTile(
-                onTap: (){},
-                leading: FaIcon(FontAwesomeIcons.laptopCode),
-                title: Text('Ofertas'),
-                trailing: FaIcon(FontAwesomeIcons.chevronRight),
-              ),
-              Divider(),
-              ListTile(
-                onTap: (){},
-                leading: FaIcon(FontAwesomeIcons.napster),
-                title: Text('Soporte'),
-                trailing: FaIcon(FontAwesomeIcons.chevronRight),
-              ),
-              Divider(),
-              ListTile(
-                onTap: (){},
-                leading: FaIcon(FontAwesomeIcons.phone),
-                title: Text('LLamar soporte'),
-                trailing: FaIcon(FontAwesomeIcons.chevronRight),
-              ),
-              Divider(),
-              ListTile(
-                onTap: (){},
-                leading: FaIcon(FontAwesomeIcons.users),
-                title: Text('Sobre nosotros'),
-                trailing: FaIcon(FontAwesomeIcons.chevronRight),
-              ),
-              Divider(),
-              ListTile(
-                onTap: (){},
-                leading: FaIcon(FontAwesomeIcons.confluence),
-                title: Text('Asjustes'),
-                trailing: FaIcon(FontAwesomeIcons.chevronRight),
-              ),
-              Divider(),
-              ListTile(
-                onTap: (){},
-                leading: FaIcon(FontAwesomeIcons.powerOff),
-                title: Text('Cerrar sesion'),
-                trailing: FaIcon(FontAwesomeIcons.chevronRight),
-              ),
-
-            ],
-          ),
-          // Container(
-          //   padding: EdgeInsets.only(top: 10.0),
-          //    child: Image(
-          //                     image: AssetImage('assets/imagen/perfil_cancha.png'),
-          //                     // width: 25.0,
-          //                     // height: 25.0,
-          //                   ),
-          // ),
-         
-        ],
-      ),
+          children: <Widget>[
+            CuadroReserva(),
+            Divider(),
+            ListTile(
+              onTap: () {},
+              leading: FaIcon(FontAwesomeIcons.moneyBillWave),
+              title: Text('Monedero'),
+              trailing: FaIcon(FontAwesomeIcons.chevronRight),
+            ),
+            Divider(),
+            ListTile(
+              onTap: () {},
+              leading: FaIcon(FontAwesomeIcons.moneyCheckAlt),
+              title: Text('Cupones'),
+              trailing: FaIcon(FontAwesomeIcons.chevronRight),
+            ),
+            Divider(),
+            ListTile(
+              onTap: () {},
+              leading: FaIcon(FontAwesomeIcons.laptopCode),
+              title: Text('Ofertas'),
+              trailing: FaIcon(FontAwesomeIcons.chevronRight),
+            ),
+            Divider(),
+            ListTile(
+              onTap: () {},
+              leading: FaIcon(FontAwesomeIcons.napster),
+              title: Text('Soporte'),
+              trailing: FaIcon(FontAwesomeIcons.chevronRight),
+            ),
+            Divider(),
+            ListTile(
+              onTap: () {},
+              leading: FaIcon(FontAwesomeIcons.phone),
+              title: Text('LLamar soporte'),
+              trailing: FaIcon(FontAwesomeIcons.chevronRight),
+            ),
+            Divider(),
+            ListTile(
+              onTap: () {},
+              leading: FaIcon(FontAwesomeIcons.users),
+              title: Text('Sobre nosotros'),
+              trailing: FaIcon(FontAwesomeIcons.chevronRight),
+            ),
+            Divider(),
+            ListTile(
+              onTap: () {},
+              leading: FaIcon(FontAwesomeIcons.confluence),
+              title: Text('Asjustes'),
+              trailing: FaIcon(FontAwesomeIcons.chevronRight),
+            ),
+            Divider(),
+            ListTile(
+              onTap: () {},
+              leading: FaIcon(FontAwesomeIcons.powerOff),
+              title: Text('Cerrar sesion'),
+              trailing: FaIcon(FontAwesomeIcons.chevronRight),
+            ),
+          ],
+        ),
+        //Column(
+        //  children: <Widget>[
+        //    Container(
+        //      padding: EdgeInsets.only(top: 10.0),
+        //      child: Image(
+        //        image: AssetImage('assets/imagen/perfil_cancha.png'),
+        //        // width: 25.0,
+        //        // height: 25.0,
+        //      ),
+        //    ),
+        //  ],
+        //),
       ],
-          
     );
   }
 }
@@ -108,35 +104,35 @@ class DatosPersonales extends StatelessWidget {
             Container(
               padding: EdgeInsets.only(left: 20),
               width: 70,
-              child: FaIcon(FontAwesomeIcons.userCircle)
+              child: FaIcon(FontAwesomeIcons.userCircle),
             ),
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  SizedBox(height: 15,),
+                  SizedBox(height: 15),
                   Text('Diego Celis'),
-                  SizedBox(height: 7,),
+                  SizedBox(height: 7),
                   Text('Masculino, 29 años'),
-                  SizedBox(height: 7,),
+                  SizedBox(height: 7),
                   Text('992293122'),
-                  SizedBox(height: 7,),
+                  SizedBox(height: 7),
                   Text('micorreo@hotmal.com'),
                 ],
-              )
+              ),
             ),
             Container(
               width: 25,
               // child: FaIcon(FontAwesomeIcons.chevronRight)
             ),
           ],
-        )
+        ),
       ),
     );
   }
 }
-class CuadroReserva extends StatelessWidget {
 
+class CuadroReserva extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -152,8 +148,14 @@ class CuadroReserva extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  FaIcon(FontAwesomeIcons.futbol,size: 45,),
-                  Text('17',style: TextStyle(fontSize: 30),),
+                  FaIcon(
+                    FontAwesomeIcons.futbol,
+                    size: 45,
+                  ),
+                  Text(
+                    '17',
+                    style: TextStyle(fontSize: 30),
+                  ),
                   Text('martes'),
                 ],
               ),
@@ -164,18 +166,33 @@ class CuadroReserva extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Text('Estado de reserva'),
-                    SizedBox(height: 10,),
-                    Text('Deportivo Futgol',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                    SizedBox(height: 10,),
-                    Text('8:00 AM',style: TextStyle(fontSize: 18),),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Deportivo Futgol',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      '8:00 AM',
+                      style: TextStyle(fontSize: 18),
+                    ),
                   ],
                 ),
               ),
             ),
             Container(
-              padding: EdgeInsets.only(top: 20,right: 15),
+              padding: EdgeInsets.only(top: 20, right: 15),
               height: double.infinity,
-              child: Text('Pediente',style: TextStyle(color: Colors.orange,fontWeight: FontWeight.bold),),
+              child: Text(
+                'Pediente',
+                style: TextStyle(
+                    color: Colors.orange, fontWeight: FontWeight.bold),
+              ),
             ),
           ],
         ),
