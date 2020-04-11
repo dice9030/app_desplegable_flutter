b4ad82c3c48e11b5540ff174b5e54e601a6db0ff
Softdynamic <softdynamic@MacBook-Pro-de-Softdynamic.local>
2020-04-11 14:11:13 -0500

    archivos

--- lib/widget/mi_perfil.dart
+++ lib/widget/mi_perfil.dart
@@ -6,45 +6,43 @@ class MiPerfil extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
-        FaIcon(
-          FontAwesomeIcons.userCircle,
-          size: 40,
+        Container(
+          child: FaIcon(FontAwesomeIcons.userCircle,size: 40,),
         ),
-        Divider(color: Colors.grey, height: 40),
-        MiPerfilTile(nombre: 'Nombre', valor: 'Diego Manuel'),
-        Divider(color: Colors.grey, height: 40),
-        MiPerfilTile(nombre: 'Apellido', valor: 'Celis Gotia'),
-        Divider(color: Colors.grey, height: 40),
-        MiPerfilTile(nombre: 'Edad', valor: '29'),
-        Divider(color: Colors.grey, height: 40),
-      ],
-    );
-  }
-}
-
-class MiPerfilTile extends StatelessWidget {
-  const MiPerfilTile({Key key, this.nombre, this.valor})
-      : assert(nombre != null),
-        assert(valor != null),
-        super(key: key);
-
-  final String nombre;
-  final String valor;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          '$nombre: ',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        SizedBox(height: 20,),
+        Divider(color: Colors.grey,),
+        SizedBox(height: 20,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text('Nombre: ',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+            Text('Diego Manuel',style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)            
+          ],
         ),
-        Text(
-          valor,
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-        )
+        SizedBox(height: 20,),
+        Divider(color: Colors.grey,),
+        SizedBox(height: 20,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text('Apellido: ',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+            Text('Celis Gotia',style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)            
+          ],
+        ),
+        SizedBox(height: 20,),
+        Divider(color: Colors.grey,),
+        SizedBox(height: 20,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text('Edad: ',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+            Text('29',style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)            
+          ],
+        ),
+        SizedBox(height: 20,),
+        Divider(color: Colors.grey,),
+        SizedBox(height: 20,),
       ],
-    );
+     );
   }
 }
